ee9fcd275d7ff6ea1fb128b9500ae9c2b69e74ad
Mauricio Wittenberg <mauricio.wittenberg@premise.com>
2023-03-23 13:50:17 -0300

    query metadata.google.internal/computeMetadata to get region
    get_default_location() first tries to get a location from a list of environment variables.
    in there is no enrironment variable set to provide a location, query the metadata service.
    any exception returns None

--- goblet_gcp_client/__init__.py
+++ goblet_gcp_client/__init__.py
@@ -1,2 +1,6 @@
 from goblet_gcp_client.client import Client  # noqa: F401
-from goblet_gcp_client.http_files import get_response, get_responses, get_replay_count  # noqa: F401
+from goblet_gcp_client.http_files import (  # noqa: F401
+    get_response,
+    get_responses,
+    get_replay_count,
+)
--- goblet_gcp_client/tests/test_client.py
+++ goblet_gcp_client/tests/test_client.py
@@ -6,6 +6,17 @@ class TestClient:
         monkeypatch.setenv("GOOGLE_PROJECT", "TEST")
         assert get_default_project() == "TEST"
 
-    def get_default_location(self, monkeypatch):
+    def test_get_default_location_environment_variable(self, monkeypatch, requests_mock):
         monkeypatch.setenv("GCLOUD_REGION", "TEST")
+        requests_mock.get(
+            "http://metadata.google.internal/computeMetadata/v1/instance/region",
+            text="us-central2",
+        )
         assert get_default_location() == "TEST"
+
+    def test_get_default_location_metadata_service(self, monkeypatch, requests_mock):
+        requests_mock.get(
+            "http://metadata.google.internal/computeMetadata/v1/instance/region",
+            text="us-central2",
+        )
+        assert get_default_location() == "us-central2"
